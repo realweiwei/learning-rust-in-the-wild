d085086baaa053fb1fab4c32b343b3fa46afeb61
Wei Wang <wei@dhcp-206-87-212-35.ubcsecure.wireless.ubc.ca>
2025-02-10 16:52:12 -0800

    hello hello cargo

---
+++ hello_cargo/target/release/deps/hello_cargo-1367233ed10a4f0b.d
@@ -0,0 +1,5 @@
+/Users/wei/Desktop/learning-rust-in-the-wild/hello_cargo/target/release/deps/hello_cargo-1367233ed10a4f0b: src/main.rs
+
+/Users/wei/Desktop/learning-rust-in-the-wild/hello_cargo/target/release/deps/hello_cargo-1367233ed10a4f0b.d: src/main.rs
+
+src/main.rs:
